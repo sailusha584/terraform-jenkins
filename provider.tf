135ce2d1ae44f0fb3fda20244c36ee182aba6dff
sailusha584 <sailusha.peddibhotla@gmail.com>
2023-11-03 14:05:55 +0000

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.18.0"
+   }
+ }
+
+ backend "s3" {
+   bucket = "tf-tuts-state"
+   key    = "state"
+   region = "eu-west-2"
+ }
+}
